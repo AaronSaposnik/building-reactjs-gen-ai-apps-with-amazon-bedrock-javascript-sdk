304b9bbfa341a13368164236c4e743489e178f47
AaronSaposnik <aaron.saposnik@franklinmonroe.com>
2024-08-19 08:46:53 -0500

    Update App.jsx

--- reactjs-gen-ai-apps/src/App.jsx
+++ reactjs-gen-ai-apps/src/App.jsx
@@ -1,29 +1,62 @@
-    import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Hub } from "aws-amplify/utils";
-import { signInWithRedirect, getCurrentUser, AuthUser } from "aws-amplify/auth";
+import { signInWithRedirect, signOut, getCurrentUser } from "aws-amplify/auth";
 
-Hub.listen("auth", ({ payload }) => {
-  switch (payload.event) {
-    case "signInWithRedirect":
-      const user = await getCurrentUser();
-      console.log(user.username);
-      break;
-    case "signInWithRedirect_failure":
-      // handle sign in failure
-      break;
-    case "customOAuthState":
-      const state = payload.data; // this will be customState provided on signInWithRedirect function
-      console.log(state);
-      break;
-  }
-});
+function App() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [customState, setCustomState] = useState(null);
 
-function handleSignInClick() {
-  signInWithRedirect({
-    provider: {custom:'Azure'}
-  });
+  useEffect(() => {
+    const unsubscribe = Hub.listen("auth", ({ payload }) => {
+      switch (payload.event) {
+        case "signInWithRedirect":
+          getUser();
+          break;
+        case "signInWithRedirect_failure":
+          setError("An error has occurred during the OAuth flow.");
+          break;
+        case "customOAuthState":
+          setCustomState(payload.data); // this is the customState provided on signInWithRedirect function
+          break;
+      }
+    });
+
+    getUser();
+
+    return unsubscribe;
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser);
+    } catch (error) {
+      console.error(error);
+      console.log("Not signed in");
+    }
+  };
+
+  return (
+    <div className="App">
+      <button onClick={() => signInWithRedirect()}>Open Hosted UI</button>
+      <button onClick={() => signInWithRedirect({ provider: {custom:'Azure'}})}>
+        Open Azure
+      </button>
+      <button onClick={() => signInWithRedirect({ provider: "Facebook"})}>
+        Open Facebook
+      </button>
+      <button onClick={() => signInWithRedirect({ provider: "Google"})}>
+        Open Google
+      </button>
+      <button onClick={() => signInWithRedirect({ provider: "Amazon" })}>
+        Open Amazon
+      </button>
+      <button onClick={() => signInWithRedirect({ provider: "Apple"})}>
+        Open Apple
+      </button>
+      <button onClick={() => signOut()}>Sign Out</button>
+      <div>{user?.username}</div>
+    </div>
+  );
 }
-
-
-
-export default handleSignInClick
